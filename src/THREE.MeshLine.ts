60befd5c49048d9e604eaefc14f3657a4d669639
sogrey <suzhaoheng2006@163.com>
2020-03-25 11:26:26 +0800

    add tsconfig.json

--- src/THREE.MeshLine.ts
+++ src/THREE.MeshLine.ts
@@ -1,19 +1,19 @@
 import * as THREE from 'three'
 
-function memcpy (src: any, srcOffset: number, dst: any, dstOffset: number, length: number) {
+function memcpy(src: any, srcOffset: number, dst: any, dstOffset: number, length: number) {
 	let i
 
 	src = src.subarray || src.slice ? src : src.buffer
 	dst = dst.subarray || dst.slice ? dst : dst.buffer
 
 	src = srcOffset ? src.subarray ?
-	src.subarray(srcOffset, length && srcOffset + length) :
-	src.slice(srcOffset, length && srcOffset + length) : src
+		src.subarray(srcOffset, length && srcOffset + length) :
+		src.slice(srcOffset, length && srcOffset + length) : src
 
 	if (dst.set) {
 		dst.set(src, dstOffset)
 	} else {
-		for (i=0; i<src.length; i++) {
+		for (i = 0; i < src.length; i++) {
 			dst[i + dstOffset] = src[i]
 		}
 	}
@@ -103,10 +103,10 @@ THREE.ShaderChunk['meshline_vert'] = [
 	'    gl_Position = finalPosition;',
 	'',
 	THREE.ShaderChunk.logdepthbuf_vertex,
-  THREE.ShaderChunk.fog_vertex && '    vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );',
-  THREE.ShaderChunk.fog_vertex,
+	THREE.ShaderChunk.fog_vertex && '    vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );',
+	THREE.ShaderChunk.fog_vertex,
 	'}'
-].join( '\r\n' );
+].join('\r\n');
 
 THREE.ShaderChunk['meshline_frag'] = [
 	'',
@@ -145,7 +145,7 @@ THREE.ShaderChunk['meshline_frag'] = [
 	'',
 	THREE.ShaderChunk.fog_fragment,
 	'}'
-].join( '\r\n' );
+].join('\r\n');
 
 
 export class MeshLine {
@@ -176,15 +176,15 @@ export class MeshLine {
 	// Used to ray cast
 	private matrixWorld = new THREE.Matrix4()
 
-	constructor () {}
+	constructor() { }
 
-	setMatrixWorld (matrixWorld: THREE.Matrix4) {
+	setMatrixWorld(matrixWorld: THREE.Matrix4) {
 		this.matrixWorld = matrixWorld
 	}
 
-	setGeometry (g: THREE.Geometry | THREE.BufferGeometry | Float32Array | number[], c: any) {
+	setGeometry(g: THREE.Geometry | THREE.BufferGeometry | Float32Array | number[], c: any) {
 		this.widthCallback = c
-	
+
 		this.positions = []
 		this.counters = []
 
@@ -198,111 +198,111 @@ export class MeshLine {
 				this.counters.push(c)
 			}
 		}
-	
-		if( g instanceof THREE.BufferGeometry ) {
+
+		if (g instanceof THREE.BufferGeometry) {
 			// read attribute positions ?
 		}
-	
-		if( g instanceof Float32Array || g instanceof Array ) {
-			for( var j = 0; j < g.length; j += 3 ) {
+
+		if (g instanceof Float32Array || g instanceof Array) {
+			for (var j = 0; j < g.length; j += 3) {
 				const c = j / g.length
-				this.positions.push( g[ j ], g[ j + 1 ], g[ j + 2 ] )
-				this.positions.push( g[ j ], g[ j + 1 ], g[ j + 2 ] )
+				this.positions.push(g[j], g[j + 1], g[j + 2])
+				this.positions.push(g[j], g[j + 1], g[j + 2])
 				this.counters.push(c)
 				this.counters.push(c)
 			}
 		}
-	
+
 		this.process()
 	}
 
-	compareV3 (a: number, b: number) {
+	compareV3(a: number, b: number) {
 		const aa = a * 6
 		const ab = b * 6
-		return (this.positions[aa] === this.positions[ab]) && ( this.positions[aa + 1] === this.positions[ab + 1]) && (this.positions[aa + 2] === this.positions[ab + 2])
-	
+		return (this.positions[aa] === this.positions[ab]) && (this.positions[aa + 1] === this.positions[ab + 1]) && (this.positions[aa + 2] === this.positions[ab + 2])
+
 	}
-	
-	copyV3 (a: number) {
+
+	copyV3(a: number) {
 		const aa = a * 6
 		return [this.positions[aa], this.positions[aa + 1], this.positions[aa + 2]]
 	}
-	
-	process () {
+
+	process() {
 		const l = this.positions.length / 6
-	
+
 		this.previous = []
 		this.next = []
 		this.side = []
 		this.width = []
 		this.indices_array = []
 		this.uvs = []
-	
+
 		for (let j = 0; j < l; j++) {
-			this.side.push( 1 )
-			this.side.push( -1 )
+			this.side.push(1)
+			this.side.push(-1)
 		}
-	
+
 		let w
-		for(let j = 0; j < l; j++) {
-			if (this.widthCallback) w = this.widthCallback( j / ( l -1 ))
+		for (let j = 0; j < l; j++) {
+			if (this.widthCallback) w = this.widthCallback(j / (l - 1))
 			else w = 1
-			this.width.push( w )
-			this.width.push( w )
+			this.width.push(w)
+			this.width.push(w)
 		}
-	
-		for(let j = 0; j < l; j++) {
-			this.uvs.push( j / ( l - 1 ), 0 )
-			this.uvs.push( j / ( l - 1 ), 1 )
+
+		for (let j = 0; j < l; j++) {
+			this.uvs.push(j / (l - 1), 0)
+			this.uvs.push(j / (l - 1), 1)
 		}
-	
+
 		let v
-	
-		if (this.compareV3( 0, l - 1 )) {
-			v = this.copyV3( l - 2 )
+
+		if (this.compareV3(0, l - 1)) {
+			v = this.copyV3(l - 2)
 		} else {
-			v = this.copyV3( 0 )
+			v = this.copyV3(0)
 		}
 
 		this.previous.push(v[0], v[1], v[2])
 		this.previous.push(v[0], v[1], v[2])
-	
+
 		for (let j = 0; j < l - 1; j++) {
-			v = this.copyV3( j )
+			v = this.copyV3(j)
 			this.previous.push(v[0], v[1], v[2])
 			this.previous.push(v[0], v[1], v[2])
 		}
-	
+
 		for (let j = 1; j < l; j++) {
 			v = this.copyV3(j)
 			this.next.push(v[0], v[1], v[2])
 			this.next.push(v[0], v[1], v[2])
 		}
-	
-		if (this.compareV3(l - 1, 0)){
+
+		if (this.compareV3(l - 1, 0)) {
 			v = this.copyV3(1)
 		} else {
 			v = this.copyV3(l - 1)
 		}
 		this.next.push(v[0], v[1], v[2])
 		this.next.push(v[0], v[1], v[2])
-	
+
 		for (let j = 0; j < l - 1; j++) {
 			let n = j * 2
 			this.indices_array.push(n, n + 1, n + 2)
 			this.indices_array.push(n + 2, n + 1, n + 3)
 		}
-	
+
 		if (!this.attributes) {
 			this.attributes = {
-				position: new THREE.BufferAttribute( new Float32Array( this.positions ), 3 ),
-				previous: new THREE.BufferAttribute( new Float32Array( this.previous ), 3 ),
-				next: new THREE.BufferAttribute( new Float32Array( this.next ), 3 ),
-				side: new THREE.BufferAttribute( new Float32Array( this.side ), 1 ),
-				width: new THREE.BufferAttribute( new Float32Array( this.width ), 1 ),
-				uv: new THREE.BufferAttribute( new Float32Array( this.uvs ), 2 ),
-				index: new THREE.BufferAttribute( new Uint16Array( this.indices_array ), 1 ),
-				counters: new THREE.BufferAttribute( new Float32Array( this.counters ), 1 )
+				position: new THREE.BufferAttribute(new Float32Array(this.positions), 3),
+				previous: new THREE.BufferAttribute(new Float32Array(this.previous), 3),
+				next: new THREE.BufferAttribute(new Float32Array(this.next), 3),
+				side: new THREE.BufferAttribute(new Float32Array(this.side), 1),
+				width: new THREE.BufferAttribute(new Float32Array(this.width), 1),
+				uv: new THREE.BufferAttribute(new Float32Array(this.uvs), 2),
+				index: new THREE.BufferAttribute(new Uint16Array(this.indices_array), 1),
+				counters: new THREE.BufferAttribute(new Float32Array(this.counters), 1)
 			}
 		} else {
 			this.attributes.position.copyArray(new Float32Array(this.positions))
@@ -320,7 +320,7 @@ export class MeshLine {
 			this.attributes.index.copyArray(new Uint16Array(this.indices_array))
 			this.attributes.index.needsUpdate = true
 		}
-	
+
 		this.geometry.addAttribute('position', this.attributes.position)
 		this.geometry.addAttribute('previous', this.attributes.previous)
 		this.geometry.addAttribute('next', this.attributes.next)
@@ -328,7 +328,7 @@ export class MeshLine {
 		this.geometry.addAttribute('width', this.attributes.width)
 		this.geometry.addAttribute('uv', this.attributes.uv)
 		this.geometry.addAttribute('counters', this.attributes.counters)
-	
+
 		this.geometry.setIndex(this.attributes.index)
 	}
 
@@ -336,7 +336,7 @@ export class MeshLine {
 	private rcRay = new THREE.Ray()
 	private rcSphere = new THREE.Sphere()
 
-	raycast (raycaster: THREE.Raycaster, intersects) {
+	raycast(raycaster: THREE.Raycaster, intersects:any[]) {
 		const precision = raycaster.linePrecision
 		const precisionSq = precision * precision
 
@@ -348,10 +348,13 @@ export class MeshLine {
 		this.rcSphere.copy(geometry.boundingSphere)
 		this.rcSphere.applyMatrix4(this.matrixWorld)
 
-		if ( raycaster.ray.intersectSphere(this.rcSphere) === false) return undefined
+		const target = new THREE.Vector3();
+		raycaster.ray.intersectSphere(this.rcSphere, target);
+
+		if (target===undefined|| target === new THREE.Vector3()) return undefined
 
 		this.rcInverseMatrix.getInverse(this.matrixWorld)
-		this.rcRay.copy( raycaster.ray ).applyMatrix4(this.rcInverseMatrix)
+		this.rcRay.copy(raycaster.ray).applyMatrix4(this.rcInverseMatrix)
 
 		const vStart = new THREE.Vector3()
 		const vEnd = new THREE.Vector3()
@@ -430,35 +433,35 @@ export class MeshLine {
 	/**
 	 * Fast method to advance the line by one position.  The oldest position is removed.
 	 */
-	advance (position: THREE.Vector3) {
+	advance(position: THREE.Vector3) {
 		const positions: any = this.attributes.position.array
 		const previous: any = this.attributes.previous.array
 		const next: any = this.attributes.next.array
 		const l: number = positions.length
-	
+
 		// Previous
 		memcpy(positions, 0, previous, 0, l)
-	
+
 		// Positions
 		memcpy(positions, 6, positions, 0, l - 6)
-	
+
 		positions[l - 6] = position.x
 		positions[l - 5] = position.y
 		positions[l - 4] = position.z
 		positions[l - 3] = position.x
 		positions[l - 2] = position.y
 		positions[l - 1] = position.z
-	
+
 		// Next
 		memcpy(positions, 6, next, 0, l - 6)
-	
-		next[l - 6]  = position.x
-		next[l - 5]  = position.y
-		next[l - 4]  = position.z
-		next[l - 3]  = position.x
-		next[l - 2]  = position.y
-		next[l - 1]  = position.z
-	
+
+		next[l - 6] = position.x
+		next[l - 5] = position.y
+		next[l - 4] = position.z
+		next[l - 3] = position.x
+		next[l - 2] = position.y
+		next[l - 1] = position.z
+
 		this.attributes.position.needsUpdate = true
 		this.attributes.previous.needsUpdate = true
 		this.attributes.next.needsUpdate = true
@@ -468,7 +471,7 @@ export class MeshLine {
 export class MeshLineMaterial extends THREE.ShaderMaterial {
 	private isMeshLineMaterial = true
 
-	constructor (parameters) {
+	constructor(parameters:any) {
 		super({
 			uniforms: Object.assign({},
 				THREE.UniformsLib.fog,
@@ -478,9 +481,9 @@ export class MeshLineMaterial extends THREE.ShaderMaterial {
 					useMap: { value: 0 },
 					alphaMap: { value: null },
 					useAlphaMap: { value: 0 },
-					color: { value: new THREE.Color( 0xffffff ) },
+					color: { value: new THREE.Color(0xffffff) },
 					opacity: { value: 1 },
-					resolution: { value: new THREE.Vector2( 1, 1 ) },
+					resolution: { value: new THREE.Vector2(1, 1) },
 					sizeAttenuation: { value: 1 },
 					near: { value: 1 },
 					far: { value: 1 },
@@ -488,157 +491,157 @@ export class MeshLineMaterial extends THREE.ShaderMaterial {
 					dashOffset: { value: 0 },
 					dashRatio: { value: 0.5 },
 					useDash: { value: 0 },
-					visibility: {value: 1 },
-					alphaTest: {value: 0 },
-					repeat: { value: new THREE.Vector2( 1, 1 ) },
+					visibility: { value: 1 },
+					alphaTest: { value: 0 },
+					repeat: { value: new THREE.Vector2(1, 1) },
 				}
 			),
-	
+
 			vertexShader: THREE.ShaderChunk.meshline_vert,
-	
+
 			fragmentShader: THREE.ShaderChunk.meshline_frag,
-	
+
 		})
 
 		this.type = 'MeshLineMaterial';
 
-		Object.defineProperties( this, {
+		Object.defineProperties(this, {
 			lineWidth: {
 				enumerable: true,
 				get: () => this.uniforms.lineWidth.value,
-				set: (value) => this.uniforms.lineWidth.value = value,
+				set: (value:any) => this.uniforms.lineWidth.value = value,
 			},
 
 			map: {
 				enumerable: true,
 				get: () => this.uniforms.map.value,
-				set: (value) => this.uniforms.map.value = value,
+				set: (value:any) => this.uniforms.map.value = value,
 			},
 
 			useMap: {
 				enumerable: true,
 				get: () => this.uniforms.useMap.value,
-				set: (value) => this.uniforms.useMap.value = value,
+				set: (value:any) => this.uniforms.useMap.value = value,
 			},
 
 			alphaMap: {
 				enumerable: true,
 				get: () => this.uniforms.alphaMap.value,
-				set: (value) => this.uniforms.alphaMap.value = value,
+				set: (value:any) => this.uniforms.alphaMap.value = value,
 			},
 
 			useAlphaMap: {
 				enumerable: true,
 				get: () => this.uniforms.useAlphaMap.value,
-				set: (value) => this.uniforms.useAlphaMap.value = value,
+				set: (value:any) => this.uniforms.useAlphaMap.value = value,
 			},
 
 			color: {
 				enumerable: true,
 				get: () => this.uniforms.color.value,
-				set: (value) => this.uniforms.color.value = value,
+				set: (value:any) => this.uniforms.color.value = value,
 			},
 
 			opacity: {
 				enumerable: true,
 				get: () => this.uniforms.opacity.value,
-				set: (value) => this.uniforms.opacity.value = value,
+				set: (value:any) => this.uniforms.opacity.value = value,
 			},
 
 			resolution: {
 				enumerable: true,
 				get: () => this.uniforms.resolution.value,
-				set: (value) => this.uniforms.resolution.value.copy( value ),
+				set: (value:any) => this.uniforms.resolution.value.copy(value),
 			},
 
 			sizeAttenuation: {
 				enumerable: true,
 				get: () => this.uniforms.sizeAttenuation.value,
-				set: (value) => this.uniforms.sizeAttenuation.value = value,
+				set: (value:any) => this.uniforms.sizeAttenuation.value = value,
 			},
 
 			near: {
 				enumerable: true,
 				get: () => this.uniforms.near.value,
-				set: (value) => this.uniforms.near.value = value,
+				set: (value:any) => this.uniforms.near.value = value,
 			},
 
 			far: {
 				enumerable: true,
 				get: () => this.uniforms.far.value,
-				set: (value) => this.uniforms.far.value = value,
+				set: (value:any) => this.uniforms.far.value = value,
 			},
 
 			dashArray: {
 				enumerable: true,
 				get: () => this.uniforms.dashArray.value,
-				set: (value) => {
+				set: (value:any) => {
 					this.uniforms.dashArray.value = value
-					this.useDash = ( value !== 0 ) ? 1 : 0
+					this.uniforms.useDash.value = (value !== 0) ? 1 : 0
 				},
 			},
-			
+
 			dashOffset: {
 				enumerable: true,
 				get: () => this.uniforms.dashOffset.value,
-				set: (value) => this.uniforms.dashOffset.value = value,
+				set: (value:any) => this.uniforms.dashOffset.value = value,
 			},
 
 			dashRatio: {
 				enumerable: true,
 				get: () => this.uniforms.dashRatio.value,
-				set: (value) => this.uniforms.dashRatio.value = value,
+				set: (value:any) => this.uniforms.dashRatio.value = value,
 			},
 
 			useDash: {
 				enumerable: true,
 				get: () => this.uniforms.useDash.value,
-				set: (value) => this.uniforms.useDash.value = value,
+				set: (value:any) => this.uniforms.useDash.value = value,
 			},
 
 			visibility: {
 				enumerable: true,
 				get: () => this.uniforms.visibility.value,
-				set: (value) => this.uniforms.visibility.value = value,
+				set: (value:any) => this.uniforms.visibility.value = value,
 			},
 
 			alphaTest: {
 				enumerable: true,
 				get: () => this.uniforms.alphaTest.value,
-				set: (value) => this.uniforms.alphaTest.value = value,
+				set: (value:any) => this.uniforms.alphaTest.value = value,
 			},
 
 			repeat: {
 				enumerable: true,
 				get: () => this.uniforms.repeat.value,
-				set: (value) => this.uniforms.repeat.value.copy( value ),
+				set: (value:any) => this.uniforms.repeat.value.copy(value),
 			},
 		})
 
-		this.setValues( parameters );
+		this.setValues(parameters);
 	}
 
-	copy (source: any) {
+	copy(source: any) {
 		THREE.ShaderMaterial.prototype.copy.call(this, source)
 
-		this.lineWidth = source.lineWidth
-		this.map = source.map
-		this.useMap = source.useMap
-		this.alphaMap = source.alphaMap
-		this.useAlphaMap = source.useAlphaMap
-		this.color.copy(source.color)
-		this.opacity = source.opacity
-		this.resolution.copy(source.resolution)
-		this.sizeAttenuation = source.sizeAttenuation
-		this.near = source.near
-		this.far = source.far
-		this.dashArray.copy(source.dashArray)
-		this.dashOffset.copy(source.dashOffset)
-		this.dashRatio.copy(source.dashRatio)
-		this.useDash = source.useDash
-		this.visibility = source.visibility
-		this.alphaTest = source.alphaTest
-		this.repeat.copy(source.repeat)
+		this.uniforms.lineWidth = source.lineWidth
+		this.uniforms.map = source.map
+		this.uniforms.useMap = source.useMap
+		this.uniforms.alphaMap = source.alphaMap
+		this.uniforms.useAlphaMap = source.useAlphaMap
+		this.uniforms.color.value.copy(source.color)
+		this.uniforms.opacity = source.opacity
+		this.uniforms.resolution.value.copy(source.resolution)
+		this.uniforms.sizeAttenuation = source.sizeAttenuation
+		this.uniforms.near = source.near
+		this.uniforms.far = source.far
+		this.uniforms.dashArray.value.copy(source.dashArray)
+		this.uniforms.dashOffset.value.copy(source.dashOffset)
+		this.uniforms.dashRatio.value.copy(source.dashRatio)
+		this.uniforms.useDash = source.useDash
+		this.uniforms.visibility = source.visibility
+		this.uniforms.alphaTest = source.alphaTest
+		this.uniforms.repeat.value.copy(source.repeat)
 
 		return this
 	}
